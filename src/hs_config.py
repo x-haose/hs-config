0197cf8f44c4059a7900f8af99adf1e4aabff5f6
昊色居士 <xhrtxh@gmail.com>
2024-01-26 17:26:28 +0800

    删除默认的字段解析代码：prepare_field_value

--- src/hs_config.py
+++ src/hs_config.py
@@ -34,7 +34,6 @@ class CustomSettingsSource(PydanticBaseSettingsSource):
 
         for field_name, field in self.settings_cls.model_fields.items():
             field_value, field_key, value_is_complex = self.get_field_value(field, field_name)
-            field_value = self.prepare_field_value(field_name, field, field_value, value_is_complex)
             if field_value is not None:
                 d[field_key] = field_value
 
